f4a432aaa2287d7f2849e7a000af15300b93cc5d
Raven Wei <weiwardraven@gmail.com>
2025-04-11 19:47:49 -0400

    refactored app to use React Router to dynamically generate links to different components of expanded app and all associated stylings

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Clear sessionStorage if the page is refreshed
+const navEntries = performance.getEntriesByType("navigation");
+if (navEntries.length > 0 && navEntries[0].type === "reload") {
+  sessionStorage.clear();
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
